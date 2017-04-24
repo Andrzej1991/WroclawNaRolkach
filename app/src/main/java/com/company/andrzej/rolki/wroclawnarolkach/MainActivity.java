ed980c938fb3c86dd08272a961add4cf2b2358ea
Andrzej1991 <andrzej.perski@hotmail.com>
2017-04-24 10:15:26 +0200

    added ad mob and firebase

--- app/src/main/java/com/company/andrzej/rolki/wroclawnarolkach/MainActivity.java
+++ app/src/main/java/com/company/andrzej/rolki/wroclawnarolkach/MainActivity.java
@@ -1,13 +1,53 @@
 package com.company.andrzej.rolki.wroclawnarolkach;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeAdMob();
+
+    }
+
+    private void initializeAdMob(){
+        MobileAds.initialize(this, "ca-app-pub-2389435775598003/7351545379");
+        mAdView = (AdView) findViewById(R.id.ad_view);
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
     }
 }
